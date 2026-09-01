56e33294db53f15dd2f2fd1f4d979828d291db60
shashank bhaskar <shashankbhaskar2002@gmail.com>
2026-09-01 22:58:51 +0530

    Connect frontend to production backend

--- ecommerce-project/frontend/src/api/axios.js
+++ ecommerce-project/frontend/src/api/axios.js
@@ -1,86 +1,66 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import axios from "axios";
 
-
 const API = axios.create({
-
-    baseURL: "http://localhost:5000/api",
-
+    baseURL: "https://ecommerce-backend-cvy4.onrender.com/api",
     withCredentials: true
-
 });
-
-
 
 // Attach Token Automatically
 
 API.interceptors.request.use(
-
     (config) => {
-
 
         const token = localStorage.getItem("token");
 
-
         if (token) {
-
-
             config.headers.Authorization = `Bearer ${token}`;
-
-
         }
 
-
         return config;
-
-
     },
 
-
     (error) => {
-
-
         return Promise.reject(error);
-
-
     }
-
 );
-
-
-
 
 // Global Response Error Handler
 
 API.interceptors.response.use(
-
     (response) => {
-
-
         return response;
-
-
     },
-
 
     (error) => {
 
-
-        if(error.response?.status === 401){
-
-
+        if (error.response?.status === 401) {
             console.log("Unauthorized User");
-
-
         }
 
-
         return Promise.reject(error);
-
-
     }
-
 );
 
-
-
 export default API;
+
--- ecommerce-project/frontend/src/services/api.js
+++ ecommerce-project/frontend/src/services/api.js
@@ -1,3 +1,1 @@
-const API_URL = "http://localhost:5000/api";
-
-export default API_URL;
+const API_URL = "https://ecommerce-backend-cvy4.onrender.com/api"; export default API_URL;
